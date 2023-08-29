012f297dd1fe3fcd4ade3cb23fe1202cbdb7af2d
YukDDong <ekfhd5537@naver.com>
2023-08-30 02:32:42 +0900

    chore: path alias 추가

---
+++ src/components/Test/Test.tsx
@@ -0,0 +1,5 @@
+function Test() {
+  return <div>하이</div>;
+}
+
+export default Test;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { resolve } from 'path';
+import path from 'path';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: { find: '@', replacement: path.resolve(__dirname, 'src') },
+  },
+});
