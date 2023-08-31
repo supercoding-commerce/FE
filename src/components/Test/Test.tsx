8380dca459d251621c07c517e28c00907331f0f5
kimseunggyu <dev.seunggyu@gmail.com>
2023-08-31 21:31:58 +0900

    feat : theme 정리 및 예제 추가

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,5 +1,29 @@
+import styled from '@emotion/styled';
+
+import { theme } from '@/styles/theme.ts';
+
 function Test() {
-  return <div>하이</div>;
+  return (
+    <div>
+      하이
+      <H0>Title</H0>
+      <H1>Title</H1>
+      <Body0>Title</Body0>
+    </div>
+  );
 }
 
 export default Test;
+
+const H0 = styled.h1`
+  ${theme.font.h0}
+  color: ${theme.color.brand}
+`;
+
+const H1 = styled.h2`
+  ${theme.font.h1}
+`;
+
+const Body0 = styled.span`
+  ${theme.font.body0}
+`;
