6d3fb432b653a23d24d439c8b7c50ad36f3e72e8
kimseunggyu <dev.seunggyu@gmail.com>
2023-09-02 01:41:54 +0900

    feat : Reting 아이콘 추가

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -2,3 +2,4 @@ export { ReactComponent as IconUser } from './User.svg';
 export { ReactComponent as IconBag } from './Bag.svg';
 export { ReactComponent as IconSearch } from './Search.svg';
 export { ReactComponent as IconClear } from './Clear.svg';
+export { ReactComponent as IconRate } from './Rate.svg';
