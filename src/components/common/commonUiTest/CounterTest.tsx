a3d42020ae6fa446d10be8e5cf0612496ea37092
heelws <162127@naver.com>
2023-09-03 17:29:36 +0900

    bug : 오류 수정 & dev conflict 확인

--- src/components/common/commonUiTest/CounterTest.tsx
+++ src/components/common/commonUiTest/CounterTest.tsx
@@ -5,7 +5,7 @@ import Counter from '@/components/common/Counter/Counter';
 //counter에 넣을 값
 const CounterTest = () => {
   const [quantity, setQuantity] = useState<number>(3);
-  const [maxQuantity, setMaxQuantity] = useState<number>(100);
+  const [maxQuantity] = useState<number>(100);
 
   //수량 변경 핸들러
   const onQuantityChange = (newQuantity: number) => {
