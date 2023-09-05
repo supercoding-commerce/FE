07ee6397294836c6528796bff3459be525992832
whtnqls124578 <whtnqls124578@naver.com>
2023-09-05 23:23:05 +0900

    feat:#43 채팅 ui 작업 및 컴포넌트 분리 완료

---
+++ src/components/chat/chatDetail/ChatRightBox.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import * as S from '../Chat.styles';
+
+const ChatRightBox = () => {
+  return (
+    <S.ChatRightBox>
+      <div className="rightbox_wrapper">
+        <span>
+          안녕하세요. 좀 도와주세요
+          <br />좀 도와주세요
+        </span>
+      </div>
+    </S.ChatRightBox>
+  );
+};
+
+export default ChatRightBox;
