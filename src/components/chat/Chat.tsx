9eec4f5674412d712f0d3573c059a1f86168c8c1
whtnqls124578 <whtnqls124578@naver.com>
2023-09-14 04:03:43 +0900

    vercel 배포 오류 수정

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import ChatDetail from '@/components/chat/ChatDetail';
+// import ChatDetail from '@/components/chat/ChatDetail';
 import ChatList from '@/components/chat/ChatList';
 import * as S from './Chat.styles';
 
