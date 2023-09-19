f77edba3b50717562ca5b03fbd4e3c0c513b7ba7
kimseunggyu <dev.seunggyu@gmail.com>
2023-09-19 22:33:36 +0900

    fix : 잘못된 프로덕트 상세 페이지 라우팅 url 변경

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -27,7 +27,7 @@ export const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: '/product/:id',
+        path: '/product/:productId',
         element: <DetailPage />,
       },
       {
