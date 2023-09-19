515af8ee739cdc0b494d6725661140fe75953336
heelws <162127@naver.com>
2023-09-19 19:22:07 +0900

    fix : detail page UI 수정

--- src/components/DetailInfo/ProductOption.styles.tsx
+++ src/components/DetailInfo/ProductOption.styles.tsx
@@ -12,6 +12,7 @@ export const Options = styled.div`
   height: 80px;
   font: ${theme.font.body0};
   padding: 0 30px;
+  margin: 4px 0;
   flex-direction: column;
   align-items: center;
   justify-content: center;
@@ -31,7 +32,6 @@ export const SelectedOptionContainer = styled.span`
   align-items: center;
   padding: 10px 30px;
   border-top: 2px solid #d8d8d8;
-  border-bottom: 2px solid #d8d8d8;
 `;
 
 export const SelectedOption = styled.p`
@@ -42,7 +42,9 @@ export const SelectedOption = styled.p`
 export const OptionPrice = styled.p`
   font: ${theme.font.body2};
   margin-right: 3px;
-  margin-left: 30px;
+  width: 100px;
+  display: flex;
+  justify-content: flex-end;
 `;
 
 export const OptionPriceContainer = styled.span`
