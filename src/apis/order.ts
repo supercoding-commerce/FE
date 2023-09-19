c3b92dc21466009aa734702d2d0c21944b50c4fa
kimseunggyu <dev.seunggyu@gmail.com>
2023-09-19 20:31:32 +0900

    feat : 구매 API 정의

--- src/apis/order.ts
+++ src/apis/order.ts
@@ -4,5 +4,5 @@ const ORDER_URL = '/v1/api/order';
 
 export const getOrders = async (): Promise<Order[]> => {
   const response = await client.get<Order[]>(`${ORDER_URL}`);
-  return response.data.map((order) => ({ ...order, options: JSON.parse(order.options) }));
+  return response.data.map((order) => ({ ...order, options: JSON.parse(order.options ?? []) }));
 };
