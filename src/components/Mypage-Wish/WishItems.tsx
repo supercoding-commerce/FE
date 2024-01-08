b38e52afcaa093c00028f87bfb9b12fdaeef2be5
heelws <162127@naver.com>
2024-01-08 17:09:05 +0900

    feat : wish mutate post & delete

--- src/components/Mypage-Wish/WishItems.tsx
+++ src/components/Mypage-Wish/WishItems.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { deleteWish, postWish } from '@/apis/wish';
 import Icon, { IconNameType } from '@/components/common/Icon';
 import { Wish } from '@/components/Mypage-Wish/WishPage';
+import { useDeleteWish, usePostWish } from '@/queries/mypage-wishList/mutation';
 import * as S from './WishItems.styles';
 
 type WishItemProps = {
@@ -14,12 +14,15 @@ export function WishItems({ item }: WishItemProps) {
   const [heart, setHeart] = useState<IconNameType>('IconFullHeart');
   const navigate = useNavigate();
 
+  const { postWishMutate } = usePostWish();
+  const { deleteWishMutate } = useDeleteWish();
+
   const changeHeartHandler = (productId: number) => {
     setHeart(heart === 'IconEmptyHeart' ? 'IconFullHeart' : 'IconEmptyHeart');
     if (heart === 'IconEmptyHeart') {
-      postWish(productId);
+      postWishMutate(productId);
     } else {
-      deleteWish(productId);
+      deleteWishMutate(productId);
     }
   };
 
